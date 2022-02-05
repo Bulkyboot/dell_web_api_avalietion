a18f047065299559e397de38366f7f91e78e0705
Ruan Bezerra da Silva <ruan.silva@lead.fortaleza>
2022-02-04 22:34:03 -0300

    Added front in the project

--- Front.Dell.LeadWeAPI/src/services/AddressService.js
+++
@@ -1,8 +0,0 @@
-import axios from 'axios';
-
-export default class ContributorsServices {
-    
-    getAddress() {
-      return axios.get("https://localhost:44384/api/v1/address");
-    }
-}
---
+++ Web/Dell.Lead.WeApi.Web/src/App.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="d-menubar">
+    <Menubar :model="items">
+      <template #start>
+        <img alt="logo" src="https://leadfortaleza.com.br/assets/logos/logo-lead-dell-horizontal.svg" height="25" class="p-mr-2">
+      </template>
+    </Menubar>
+  </div>
+  <router-view />
+</template>
+
+<script>
+import items from './data/menu';
+
+  export default {
+    name: 'App',
+    data() {
+      return {
+        items
+      }
+    }
+  }
+</script>
