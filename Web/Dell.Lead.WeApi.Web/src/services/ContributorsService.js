ab63dc9ca1c81f643bae33417ec4ed741162dc7e
Ruan Bezerra da Silva <ruan.silva@lead.fortaleza>
2022-02-04 22:43:44 -0300

    Bringing the Backend push method

--- Web/Dell.Lead.WeApi.Web/src/services/ContributorsService.js
+++ Web/Dell.Lead.WeApi.Web/src/services/ContributorsService.js
@@ -7,4 +7,7 @@ export default class ContributorsServices {
   postContributors(contributors = {}) {
     return axios.post('https://localhost:44373/api/v1/contributors', contributors)
   }
+  putContributors(contributors = {}) {
+    return axios.put('https://localhost:44373/api/v1/employees', contributors)
+  }
 }
